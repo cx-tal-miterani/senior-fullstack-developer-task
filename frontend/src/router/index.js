8804a5f6bed7d362320b7e80436c45588e58d46a
Tal Miterani <tal.miterani@checkmarx.com>
2026-01-04 15:29:32 +0200

    set auth check on each route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router"
 import Login from "../views/Login.vue"
+import store from "../store"
 
 const routes = [
 	{
@@ -12,22 +13,37 @@ const routes = [
 		name: "Home",
 		// Lazy loading for better performance
 		component: () => import("../views/Home.vue"),
+		meta: { requiresAuth: true },
 	},
 	{
 		path: "/admin",
 		name: "Admin",
 		component: () => import("../views/AdminView.vue"),
+		meta: { requiresAuth: true, roles: ["Admin"] },
 	},
 	{
 		path: "/editor",
 		name: "Editor",
 		component: () => import("../views/EditorView.vue"),
+		meta: { requiresAuth: true, roles: ["Editor", "Admin"] },
 	},
 ]
 
 const router = createRouter({
 	history: createWebHistory(),
 	routes,
+})
+
+router.beforeEach((to, from, next) => {
+	const userRoles = store.getters.roles
+
+	if (to.meta.requiresAuth && !store.state.user) {
+		next("/")
+	} else if (to.meta.roles && !to.meta.roles.some((role) => userRoles.includes(role))) {
+		next("/home")
+	} else {
+		next()
+	}
 })
 
 export default router
